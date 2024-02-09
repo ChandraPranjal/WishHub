e92094e6cd61c4ffcb762bbe09d8726d71f71009
ChandraPranjal <iampranjalchandra@gmail.com>
2024-02-09 12:05:47 +0530

    _id tranformation to id

--- WishHub-Backend/models/brand.model.js
+++ WishHub-Backend/models/brand.model.js
@@ -1,21 +1,40 @@
 const mongoose = require("mongoose");
 
-const brandSchema = new mongoose.Schema({
-  value: {
-    type: String,
-    required: true,
-    unique: true,
+const brandSchema = new mongoose.Schema(
+  {
+    value: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    label: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    checked: {
+      type: Boolean,
+      default: false,
+    },
   },
-  label: {
-    type: String,
-    required: true,
-    unique: true,
-  },
-  checked: {
-    type: Boolean,
-    default: false,
-  },
-});
+
+  {
+    toJSON: {
+      virtuals: true,
+      versionKey: false,
+      transform: function (ret, doc) {
+        delete ret._id;
+      },
+    },
+    virtuals: {
+      id: {
+        get() {
+          return this._id;
+        },
+      },
+    },
+  }
+);
 
 const Brand = new mongoose.model("Brand", brandSchema);
 
--- WishHub-Backend/models/category.model.js
+++ WishHub-Backend/models/category.model.js
@@ -1,21 +1,39 @@
 const mongoose = require("mongoose");
 
-const categorySchema = new mongoose.Schema({
-  value: {
-    type: String,
-    required: true,
-    unique: true,
+const categorySchema = new mongoose.Schema(
+  {
+    value: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    label: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    checked: {
+      type: Boolean,
+      default: false,
+    },
   },
-  label: {
-    type: String,
-    required: true,
-    unique: true,
-  },
-  checked: {
-    type: Boolean,
-    default: false,
-  },
-});
+  {
+    toJSON: {
+      virtuals: true,
+      versionKey: false,
+      transform: (doc, ret) => {
+        delete ret._id;
+      },
+    },
+    virtuals: {
+      id: {
+        get() {
+          return this._id;
+        },
+      },
+    },
+  }
+);
 
 const Category = new mongoose.model("Category", categorySchema);
 
